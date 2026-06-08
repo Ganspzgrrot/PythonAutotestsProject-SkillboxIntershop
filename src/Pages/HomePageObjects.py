8b515c403e8c176aa8292aba5126891c3e4d578f
ganspzgrrot <flugdiffraktiojakajzeug@gmail.com>
2026-06-08 04:10:20 +0300

    Обновление файла Page Object Method

--- src/Pages/HomePageObjects.py
+++ src/Pages/HomePageObjects.py
@@ -5,66 +5,88 @@ from src.SupportFunctions.WaitFunctions.wait_until_on_xpath import wait_xpath_el
 
 class HomePageObjects:
     def __init__(self, driver):
-        #Для запуска браузера:
+        #1-Для запуска браузера:
         self.driver = driver
         self.MAIN_PAGE_URL = "https://intershop4.skillbox.ru"
-        #Текстовые ссылки:
+        #2-Карточки-промоакции "Книги", "Планшеты", "Фотоаппараты":
+        self.PROMOTIONS_BOOKS = "//aside[@id='accesspress_storemo-2']//a//div[@class='caption wow fadeIn']"
+        self.PROMOTIONS_PADS = "//aside[@id='accesspress_storemo-3']//a//div[@class='caption wow fadeIn']"
+        self.PROMOTIONS_CAMERAS = "//aside[@id='accesspress_storemo-4']//a//div[@class='caption wow fadeIn']"
+        #2-Для валидации URL из промоакций в верху сайта интершоп4:
+        self.CORRECT_URL_PROMOTIONS = [
+            "https://intershop4.skillbox.ru/product-category/catalog/electronics/watch/",
+            "https://intershop4.skillbox.ru/product-category/catalog/appliances/",
+            "https://intershop4.skillbox.ru/product-category/catalog/electronics/photo_video/"
+        ]
+        #3-Текстовые ссылки:
         self.ALL_PRODUCTS_LINK = "//a[contains(text(),'Все товары')]"
         self.MAIN_PAGE_LINK = "//li[@class='page_item page-item-39 current_page_item']//a[contains(text(),'Главная')]"
         self.CART_LINK = "//li[@class='page_item page-item-20']//a[contains(text(),'Корзина')]"
         self.MY_ACCOUNT_LINK = "//li[@class='page_item page-item-22']//a[contains(text(),'Мой аккаунт')]"
         self.PLACING_ORDER_LINK = "//li[contains(@class,'page_item page-item-24')]//a[contains(text(),'Оформление заказа')]"
         self.REGISTRATION_LINK = "//a[contains(text(),'Регистрация')]"
-        #Для валидации:
-        self.CORRECT_URL = [
+        #3-Для валидации URL из ссылок подвала сайта:
+        self.CORRECT_URL_LINKS = [
             "https://intershop4.skillbox.ru/shop/", "https://intershop4.skillbox.ru/",
             "https://intershop4.skillbox.ru/cart/", "https://intershop4.skillbox.ru/my-account/",
             "https://intershop4.skillbox.ru/cart/", "https://intershop4.skillbox.ru/register/"
         ]
-
+    #Запуск браузера:
     def open(self):
         with allure.step('Открыть главную страницу пиццерии https://pizzeria.skillbox.cc'):
             self.driver.get(self.MAIN_PAGE_URL)
-
     def max_win(self):
         self.driver.maximize_window()
-
+    #Кликабельность и валидация всех промоакций в верху главной странице сайта интершоп4:
+    def click_and_validate_promotions_books(self):
+        previous_url = self.driver.current_url
+        wait_xpath_element(self.driver, self.PROMOTIONS_BOOKS).click()
+        cur_url = self.driver.current_url
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_PROMOTIONS[0]
+        self.driver.get(self.MAIN_PAGE_URL)
+    def click_and_validate_promotions_pads(self):
+        previous_url = self.driver.current_url
+        wait_xpath_element(self.driver, self.PROMOTIONS_PADS).click()
+        cur_url = self.driver.current_url
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_PROMOTIONS[1]
+        self.driver.get(self.MAIN_PAGE_URL)
+    def click_and_validate_promotions_cameras(self):
+        previous_url = self.driver.current_url
+        wait_xpath_element(self.driver, self.PROMOTIONS_CAMERAS).click()
+        cur_url = self.driver.current_url
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_PROMOTIONS[2]
+    #Кликабельность и валидация всех ссылок в подвале сайта:
     def click_and_validate_AllProducts_link(self):
         previous_url = self.driver.current_url
         wait_xpath_element(self.driver, self.ALL_PRODUCTS_LINK).click()
         cur_url = self.driver.current_url
-        assert cur_url != previous_url and cur_url == self.CORRECT_URL[0]
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_LINKS[0]
         self.driver.get(self.MAIN_PAGE_URL)
-
     def click_and_validate_MainPage_link(self):
         wait_xpath_element(self.driver, self.MAIN_PAGE_LINK).click()
         cur_url = self.driver.current_url
-        assert cur_url == self.CORRECT_URL[1]
-
+        assert cur_url == self.CORRECT_URL_LINKS[1]
     def click_and_validate_Cart_link(self):
         previous_url = self.driver.current_url
         wait_xpath_element(self.driver, self.CART_LINK).click()
         cur_url = self.driver.current_url
-        assert previous_url != cur_url and cur_url == self.CORRECT_URL[2]
+        assert previous_url != cur_url and cur_url == self.CORRECT_URL_LINKS[2]
         self.driver.get(self.MAIN_PAGE_URL)
-
     def click_and_validate_MyAccount_link(self):
         previous_url = self.driver.current_url
         wait_xpath_element(self.driver, self.MY_ACCOUNT_LINK).click()
         cur_url = self.driver.current_url
-        assert cur_url != previous_url and cur_url == self.CORRECT_URL[3]
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_LINKS[3]
         self.driver.get(self.MAIN_PAGE_URL)
-
     def click_and_validate_PlacingOrder_link(self):
         previous_url = self.driver.current_url
         wait_xpath_element(self.driver, self.PLACING_ORDER_LINK).click()
         cur_url = self.driver.current_url
-        assert cur_url != previous_url and cur_url == self.CORRECT_URL[4]
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_LINKS[4]
         self.driver.get(self.MAIN_PAGE_URL)
-
     def click_and_validate_Registration_link(self):
         previous_url = self.driver.current_url
         wait_xpath_element(self.driver, self.REGISTRATION_LINK).click()
         cur_url = self.driver.current_url
-        assert cur_url != previous_url and cur_url == self.CORRECT_URL[5]
+        assert cur_url != previous_url and cur_url == self.CORRECT_URL_LINKS[5]
         self.driver.get(self.MAIN_PAGE_URL)
